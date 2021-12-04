d04e7dea564a55339f272b4741aeca9dbf845a93
Layla <shinylayla05@gmail.com>
2021-12-04 23:28:50 +0900

    UpdateTest(api Test)

--- src/test/java/api/board/controller/PostApiControllerTest.java
+++ src/test/java/api/board/controller/PostApiControllerTest.java
@@ -1,6 +1,7 @@
 package api.board.controller;
 
 import api.board.controller.web.PostSaveDto;
+import api.board.controller.web.PostUpdateRequestDto;
 import api.board.domain.Posts;
 import api.board.repository.PostRepository;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -38,7 +41,7 @@ class PostApiControllerTest {
         PostSaveDto postSaveDto = PostSaveDto.builder()
                 .title(title)
                 .content(content)
-                .writeDateTime(LocalDateTime.now())
+                //.writeDateTime(LocalDateTime.now())
                 .build();
 
         String url = "http://localhost:" + port + "/api/post";
@@ -52,7 +55,38 @@ class PostApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
 
+    }
 
+    @Test
+    public void 포스트_수정_테스트() throws Exception {
+        Posts save = postRepository.save(Posts.builder()
+                .title("제목")
+                .content("내용")
+                .writeDateTime(LocalDateTime.now())
+                .build());
+
+        Long getId = save.getId();
+        String changeTitle = "title2";
+        String changeContent = "content2";
+
+        PostUpdateRequestDto updateRequestDto = PostUpdateRequestDto.builder()
+                .title(changeTitle)
+                .content(changeContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/post/" + getId;
+        //요청 혹은 응답에 해당하는 객체를 request로 받아옴
+        HttpEntity<PostUpdateRequestDto> requestDtoHttpEntity = new HttpEntity<>(updateRequestDto);
+
+        //restTemplate를 통해 url, REST타입, entity, 반환받을 타입.class
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestDtoHttpEntity, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> findAll = postRepository.findAll();
+        assertThat(findAll.get(0).getTitle()).isEqualTo(changeTitle);
+        assertThat(findAll.get(0).getContent()).isEqualTo(changeContent);
 
     }
 
